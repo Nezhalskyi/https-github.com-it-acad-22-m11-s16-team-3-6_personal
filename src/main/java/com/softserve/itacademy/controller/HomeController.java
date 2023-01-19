b937965b6b4bf9c18508b95812b65a07a77464ee
Anatoliy Bobryshov <a.bobryshov@proton.me>
2023-01-19 06:50:31 +0200

    Implement authorization for HomeController

--- src/main/java/com/softserve/itacademy/controller/HomeController.java
+++ src/main/java/com/softserve/itacademy/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.repository.UserRepository;
 import com.softserve.itacademy.security.Utilities;
 import com.softserve.itacademy.service.UserService;
@@ -7,6 +8,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -23,9 +26,13 @@ public class HomeController {
     @GetMapping({"/", "home"})
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     public String home(Model model) {
-        model.addAttribute("users", userService.getAll());
+        List<User> users = userService.getAll();
+        if (!Utilities.isAdmin()) {
+            users.removeIf(u -> u.getId() != Utilities.getUserDetails().getId());
+        }
+        model.addAttribute("users", users);
         model.addAttribute("userName", Utilities.getUserName());
-        model.addAttribute("userId",Utilities.getUserDetails().getId());
+        model.addAttribute("userId", Utilities.getUserDetails().getId());
         return "home";
     }
 }
